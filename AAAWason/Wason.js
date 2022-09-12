d44e34bb1e1fb3794d3b39933508a28d059c6250
Rumpler <113324214+Rumpler@users.noreply.github.com>
2022-09-12 13:10:25 -0500

    Add files via upload

---
+++ AAAWason/Wason.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById("icon-burger-mobile");
+const nav = document.getElementById("cont-nav-mobile");
+const wason = document.getElementById("cont-img-barber");
+
+
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("MostrarMenuJs")       
+})  
